b9925470a64cfb6e3a961fad0fc59d67b182a96f
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-13 15:19:25 +0300

    wrap the onCompletedClickHandler function in useCallback

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -40,7 +40,7 @@ export const Todolist = React.memo((props: PropsType) => {
 
     const onAllClickHandler = useCallback(() => props.changeFilter('all', props.id), []);
     const onActiveClickHandler = useCallback(() => props.changeFilter('active', props.id), []);
-    const onCompletedClickHandler = () => props.changeFilter('completed', props.id);
+    const onCompletedClickHandler = useCallback(() => props.changeFilter('completed', props.id), []);
 
     let tasksForTodolist = props.tasks
 
