6bc552bf39b4501840e2a184118b5d468ffaf6fc
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-20 15:01:27 +0300

    Added ReduxStoreProviderDecorator

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Meta, Story} from '@storybook/react';
 import AppWithRedux from './AppWithRedux';
+import {ReduxStoreProviderDecorator} from './stories/decorators/ReduxStoreProviderDecorator';
 
 export default {
     title: 'Todolist/AppWithRedux',
     component: AppWithRedux,
-    argTypes: {},
+    decorators: [ReduxStoreProviderDecorator]
 } as Meta
 
 const Template: Story = (args) => <AppWithRedux {...args} />;
