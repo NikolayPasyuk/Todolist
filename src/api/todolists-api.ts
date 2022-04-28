eac4c1ef436cc807833a7558014ccdf28013b280
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-28 16:06:24 +0300

    deleted variable promise

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -49,23 +49,18 @@ type GetTasksResponse = {
 
 export const todolistsAPI = {
     getTodolist() {
-        const promise = instance.get<TodolistType[]>('todo-lists')
-        return promise
+        return instance.get<TodolistType[]>('todo-lists')
     },
     createTodolist(tilte: string) {
-        const promise = instance.post<ResponseType<{ item: TodolistType }>>('todo-lists', {title: tilte})
-        return promise
+        return instance.post<ResponseType<{ item: TodolistType }>>('todo-lists', {title: tilte})
     },
     deleteTodolist(id: string) {
-        const promise = instance.delete<ResponseType<{}>>(`todo-lists/${id}`)
-        return promise
+        return instance.delete<ResponseType<{}>>(`todo-lists/${id}`)
     },
     updateTodolist(id: string, title: string) {
-        const promise = instance.put<ResponseType<{}>>(`todo-lists/${id}`, {title: title})
-        return promise
+        return instance.put<ResponseType<{}>>(`todo-lists/${id}`, {title: title})
     },
     getTasks(todolistId: string) {
-        const promise = instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`)
-        return promise
+        return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`)
     }
 }
