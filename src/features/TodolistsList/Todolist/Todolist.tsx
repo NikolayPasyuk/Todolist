9e13336c25a1d2d7fe4810dd108c32d33c52d476
NikolayPasyuk <kolyan-1812@mail.ru>
2022-05-05 22:12:34 +0300

    created the Task folder and moved

--- src/features/TodolistsList/Todolist/Todolist.tsx
+++ src/features/TodolistsList/Todolist/Todolist.tsx
@@ -3,7 +3,7 @@ import {AddItemForm} from '../../../components/AddItemForm/AddItemForm';
 import {EditableSpan} from '../../../components/EditableSpan/EditableSpan';
 import {Button, IconButton} from '@material-ui/core';
 import {Delete} from '@material-ui/icons';
-import {Task} from '../../../Task';
+import {Task} from './Task/Task';
 import {TaskStatuses, TaskType} from '../../../api/todolists-api';
 import {FilterValuesType} from '../../../state/todolists-reducer';
 import {useDispatch} from 'react-redux';
