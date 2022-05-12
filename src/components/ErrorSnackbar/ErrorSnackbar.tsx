f00c62b1aa31e6855e6d501c2ace717aa16bce01
NikolayPasyuk <kolyan-1812@mail.ru>
2022-05-12 19:02:12 +0300

    dispatch setErrorAC

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, {AlertProps} from '@mui/material/Alert';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from '../../app/store';
+import {setErrorAC} from '../../app/app-reducer';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
     props,
@@ -13,19 +14,21 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 
 export function ErrorSnackbar() {
     // const [open, setOpen] = React.useState(true);
+    const error = useSelector<AppRootStateType, string | null>(state => state.app.error)
+    const dispatch = useDispatch()
 
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
             return;
         }
+        dispatch(setErrorAC(null))
         // setOpen(false);
     }
 
-    const error = useSelector<AppRootStateType, string | null>(state => state.app.error)
     const isOpen = error !== null
 
     return (
-        <Snackbar open={isOpen} autoHideDuration={6000} onClose={handleClose}>
+        <Snackbar open={isOpen} autoHideDuration={3000} onClose={handleClose}>
             <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
                 {error}
             </Alert>
