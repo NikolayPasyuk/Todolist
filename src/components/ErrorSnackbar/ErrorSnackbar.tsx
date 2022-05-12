764c9bdd6189cba8b754d16fb98199726e49682d
NikolayPasyuk <kolyan-1812@mail.ru>
2022-05-12 16:25:42 +0300

    deleted alert

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -36,10 +36,6 @@ export function ErrorSnackbar() {
                     This is a success message!
                 </Alert>
             </Snackbar>
-            <Alert severity="error">This is an error message!</Alert>
-            <Alert severity="warning">This is a warning message!</Alert>
-            <Alert severity="info">This is an information message!</Alert>
-            <Alert severity="success">This is a success message!</Alert>
         </Stack>
     );
 }
