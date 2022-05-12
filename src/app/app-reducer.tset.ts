ad9622db13431f1c7f4f04057a47b960353e3466
NikolayPasyuk <kolyan-1812@mail.ru>
2022-05-12 18:33:00 +0300

    wrote a test 'correct status should be set'

--- src/app/app-reducer.tset.ts
+++ src/app/app-reducer.tset.ts
@@ -14,5 +14,11 @@ test('correct error message should be set', () => {
     const endState = appReducer(startState, setErrorAC('some error'))
 
     expect(endState.error).toBe('some error');
+});
 
+test('correct status should be set', () => {
+
+    const endState = appReducer(startState, setStatusAC('loading'))
+
+    expect(endState.status).toBe('loading');
 });
