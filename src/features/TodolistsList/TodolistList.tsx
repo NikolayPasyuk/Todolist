c937bdb46e7e31499aca9606996fedbe0ff2c856
NikolayPasyuk <kolyan-1812@mail.ru>
2022-05-14 12:16:45 +0300

    did the destructuring

--- src/features/TodolistsList/TodolistList.tsx
+++ src/features/TodolistsList/TodolistList.tsx
@@ -20,7 +20,7 @@ type PropsType = {
     demo?: boolean
 }
 
-export const TodolistsList: React.FC = () => {
+export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
     const todolists = useSelector<AppRootStateType, Array<TodolistDomainType>>(state => state.todolists)
     const tasks = useSelector<AppRootStateType, TasksStateType>(state =>
         state.tasks)
