f87982b07a7536ea373d01634e879783a1b46a36
NikolayPasyuk <kolyan-1812@mail.ru>
2022-05-24 20:31:05 +0300

    password validation,must be at least 2 characters.

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -30,6 +30,11 @@ export const Login = () => {
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
                 errors.email = 'Invalid email address';
             }
+            if (!values.password) {
+                errors.password = 'Required';
+            } else if (values.password.length < 2) {
+                errors.password = 'Must be at least 2 characters';
+            }
             return errors;
         },
         onSubmit: values => {
