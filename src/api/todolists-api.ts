6f3bd5a305dcc6c351c04b22a7779139c251d20d
NikolayPasyuk <kolyan-1812@mail.ru>
2022-05-28 18:54:01 +0300

    refactoring

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
+        'API-KEY': 'd0317f6a-b584-4afe-8a81-d7b60d153a6b'
     }
 })
 
